52f1081616679e48c7e836fd8defb1a3fd74929d
BenDejardin <73031539+BenDejardin@users.noreply.github.com>
2023-10-26 10:52:28 +0200

    project initiation

---
+++ migrations/Version20231013114519.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231013114519 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, raison_sociale VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, dimension VARCHAR(20) NOT NULL, prix NUMERIC(7, 2) NOT NULL, marque VARCHAR(50) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, client_id INT DEFAULT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, nom VARCHAR(50) NOT NULL, prenom VARCHAR(50) NOT NULL, UNIQUE INDEX UNIQ_1D1C63B3E7927C74 (email), INDEX IDX_1D1C63B319EB6921 (client_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE utilisateur ADD CONSTRAINT FK_1D1C63B319EB6921 FOREIGN KEY (client_id) REFERENCES client (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE utilisateur DROP FOREIGN KEY FK_1D1C63B319EB6921');
+        $this->addSql('DROP TABLE client');
+        $this->addSql('DROP TABLE produit');
+        $this->addSql('DROP TABLE utilisateur');
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Client;
+use App\Entity\Produit;
+use App\Entity\Utilisateur;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $data = [
+            'clients' => [
+                ['raison_sociale' => 'Client A'],
+                ['raison_sociale' => 'Client B'],
+                ['raison_sociale' => 'Client C'],
+            ],
+            'produits' => [
+                ['nom' => 'Produit 1', 'dimension' => '10x10', 'prix' => '100.00', 'marque' => 'Marque A', 'description' => 'Description du Produit 1'],
+                ['nom' => 'Produit 2', 'dimension' => '15x20', 'prix' => '150.00', 'marque' => 'Marque B', 'description' => 'Description du Produit 2'],
+                ['nom' => 'Produit 3', 'dimension' => '8x12', 'prix' => '80.00', 'marque' => 'Marque C', 'description' => 'Description du Produit 3'],
+            ],
+            'utilisateurs' => [
+                ['nom' => 'John', 'prenom' => 'Doe', 'email' => 'john.doe@email.com', 'mot_de_passe' => 'motdepasse1', 'role' => 'Utilisateur', 'client_id' => 1],
+                ['nom' => 'Jane', 'prenom' => 'Smith', 'email' => 'jane.smith@email.com', 'mot_de_passe' => 'motdepasse2', 'role' => 'Admin', 'client_id' => 2],
+                ['nom' => 'Alice', 'prenom' => 'Johnson', 'email' => 'alice.johnson@email.com', 'mot_de_passe' => 'motdepasse3', 'role' => 'Utilisateur', 'client_id' => 3],
+            ],
+        ];
+
+        foreach ($data['clients'] as $clientData) {
+            $client = new Client();
+            $client->setRaisonSociale($clientData['raison_sociale']);
+            $manager->persist($client);
+        }
+
+        foreach ($data['produits'] as $produitData) {
+            $produit = new Produit();
+            $produit->setNom($produitData['nom']);
+            $produit->setDimension($produitData['dimension']);
+            $produit->setPrix($produitData['prix']);
+            $produit->setMarque($produitData['marque']);
+            $produit->setDescription($produitData['description']);
+            $manager->persist($produit);
+        }
+
+        foreach ($data['utilisateurs'] as $utilisateurData) {
+            $utilisateur = new Utilisateur();
+            $utilisateur->setNom($utilisateurData['nom']);
+            $utilisateur->setPrenom($utilisateurData['prenom']);
+            $utilisateur->setEmail($utilisateurData['email']);
+            $utilisateur->setPassword($utilisateurData['mot_de_passe']);
+            $utilisateur->setRoles([$utilisateurData['role']]);
+
+            $client = $manager->getRepository(Client::class)->find($utilisateurData['client_id']);
+            $utilisateur->setClient($client);
+
+            $manager->persist($utilisateur);
+        }
+
+        $manager->flush();
+    }
+}
